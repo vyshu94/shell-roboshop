a6eae68ee279b038b25ce3b48178a4e0258972c5
vyshu94 <vyshnavikeerthipati@gmail.com>
2026-04-19 21:02:36 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -75,7 +75,7 @@ VALIDATE $? "starting catalogue"
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
 
-INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOF("catalogue")')
+INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js
     VALIDATE $? "Loading Products"
