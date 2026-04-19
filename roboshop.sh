b02410789eba846b034d4f809f6f79e304116641
vyshu94 <vyshnavikeerthipati@gmail.com>
2026-04-19 17:28:34 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-instance_id = $( aws ec2 run-instances \
+INSTANCE_ID= $( aws ec2 run-instances \
    --image-id $AMI_ID \
    --instance-type "t3.micro" \
    --security-group-ids $SG_ID \
@@ -16,14 +16,14 @@ instance_id = $( aws ec2 run-instances \
    if [ $instance == frontend]; then
        IP=$(
             aws ec2 describe-instances \
-            --instance-ids $instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
     else
         IP=$(
             aws ec2 describe-instances \
-            --instance-ids $instance_id \
+            --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text  
         )   
