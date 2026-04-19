850680f230baa521e0fe92c9cc9c666dcf0f4ef6
vyshu94 <vyshnavikeerthipati@gmail.com>
2026-04-19 20:08:07 +0530

    actions

---
+++ catalogue.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check root user or not
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>LOGS_FILE
+VALIDATE $? "disabled the default node version" 
+
+dnf module enable nodejs:20 -y &>>LOGS_FILE
+VALIDATE $? "Enabled nodejs version 20" 
+
+dnf install nodejs -y &>>LOGS_FILE
+VALIDATE $? "Installed nodejs"
+
+#creating system user
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+
+mkdir /app
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  
+VALIDATE $? "Downloading Code"
