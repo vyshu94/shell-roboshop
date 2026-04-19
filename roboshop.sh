33c3d32e58fe0b26a7230990751060b12a2efa65
vyshu94 <vyshnavikeerthipati@gmail.com>
2026-04-19 18:09:42 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ INSTANCE_ID=$( aws ec2 run-instances \
    --image-id $AMI_ID \
    --instance-type "t3.micro" \
    --security-group-ids $SG_ID \
-   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
    --query 'Instances[0].InstanceId' \
    --output text )
 
