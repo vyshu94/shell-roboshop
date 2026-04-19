fce57eb95ae0a74af82c2d77cdf9f603fe3a24ae
vyshu94 <vyshnavikeerthipati@gmail.com>
2026-04-19 17:56:52 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -5,15 +5,15 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-INSTANCE_ID= $( aws ec2 run-instances \
+INSTANCE_ID=$( aws ec2 run-instances \
    --image-id $AMI_ID \
    --instance-type "t3.micro" \
    --security-group-ids $SG_ID \
-   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
    --query 'Instances[0].InstanceId' \
    --output text )
 
-   if [ $instance == frontend]; then
+   if [ $instance == "frontend"]; then
        IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
